322a7d746df0f1bd5b6ee5db79f117a2f72c4a7c
moshe vilner <shootermv@gmail.com>
2021-08-17 18:08:07 +0300

    add heart icon

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,12 +1,13 @@
 import React, { useEffect, useState } from "react";
 import {View, StyleSheet, Text } from "react-native";
-import { Button, Center, NativeBaseProvider } from "native-base"
+import { Button, Icon } from "native-base"
 import colors from "../constants/colors";
-
+import { MaterialCommunityIcons, Ionicons } from "@expo/vector-icons"
 const Card = ({ item, onButtonPress, buttonText = "" }) => (
   <View style={styles.item}>
     <Text style={styles.title}>{item.title}</Text>
     <Button
+    startIcon={<Icon as={MaterialCommunityIcons} name="heart" size={5} color="red"/>}
       onPress={() => {
         onButtonPress(item);
       }}>{buttonText}</Button>
