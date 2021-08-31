358ef0495fefedeaf911ad13ca1bc27583f3881d
moshe vilner <shootermv@gmail.com>
2021-08-31 17:15:57 +0300

    refactor Post item

---
+++ src/screens/Posts/components/Item.js
@@ -0,0 +1,30 @@
+import React from "react";
+import { useToast } from "native-base";
+
+import Card from "../../../components/Card";
+import { saveValueToStore } from "../../../utils/FavoritesStore";
+
+const Item = ({ item }) => {
+  const toast = useToast();
+  return (
+    <Card
+      item={item}
+      onButtonPress={async () => {
+        try {
+          await saveValueToStore(item);
+          toast.show({
+            title: "Saved At Favorites",
+          });
+        } catch (er) {
+          toast.show({
+            title: "Already Exsist",
+            status: "error",
+          });
+        }
+      }}
+      buttonText="Save to Favorites"
+    />
+  );
+};
+
+export default Item;
--- src/screens/Posts/index.js
+++ src/screens/Posts/index.js
@@ -1,52 +1,28 @@
 import React from "react";
-import { SafeAreaView, StyleSheet, StatusBar } from "react-native";
-import { Heading, Center, useToast, Spinner, FlatList } from "native-base";
+import { StyleSheet, StatusBar } from "react-native";
+import { Heading, Spinner, FlatList } from "native-base";
 
-import Card from "../../components/Card";
+import Item from "./components/Item";
 
 import { Toast } from "../../components/Toast";
-import { saveValueToStore } from "../../utils/FavoritesStore";
 import { usePostsData } from "../../utils/api";
 import ColorCenter from "../../components/ColorableCenter";
 
-const renderItem = ({ item, toast }) => {
-  return (
-    <Card
-      item={item}
-      onButtonPress={async () => {
-        try{
-          await saveValueToStore(item);
-          toast.show({
-            title: "Saved At Favorites",
-          });
-        } catch(er) {
-          toast.show({
-            title: "Already Exsist",
-            status: "error",
-          });
-        }
-      }}
-      buttonText="Save to Favorites"
-    />
-  );
-};
-
 const Posts = ({ route }) => {
   const { data, error, isFetching } = usePostsData(route.params.category);
-  const toast = useToast();
-  if (isFetching) return   <Spinner accessibilityLabel="Loading posts" />;
+
+  if (isFetching) return <Spinner accessibilityLabel="Loading posts" />;
   if (error) return <Toast text="some error happen" />;
   return (
     <ColorCenter>
-      
       <Heading>{`Posts of "${route.params.category}" category`}</Heading>
-     
+
       <FlatList
         data={data}
-        renderItem={({item}) => renderItem({item, toast})}
+        renderItem={({ item }) => <Item item={item} />}
         keyExtractor={(item) => `${item.id}`}
       />
-   </ColorCenter>
+    </ColorCenter>
   );
 };
 
