31c1eceb75ad23a8d5fd562ae577591604a6069c
moshe vilner <shootermv@gmail.com>
2021-09-02 18:26:10 +0300

    refactor: item aslo moved to its own file

---
+++ src/screens/Favorites/components/Item.js
@@ -0,0 +1,35 @@
+import React from "react";
+import { Box, Text, Pressable, HStack, Avatar, useColorMode } from "native-base";
+
+const Item = ({ item, index }) => {
+  const { colorMode } = useColorMode();
+  return (
+    <Box>
+      <Pressable
+        onPress={() => console.log("You touched me")}
+        alignItems="center"
+        bg={colorMode === "dark" ? "dark.200" : "white"}
+        borderBottomColor="trueGray.200"
+        borderBottomWidth={1}
+        justifyContent="center"
+        height={50}
+        underlayColor={"#AAA"}
+        _pressed={{
+          bg: "trueGray.200",
+        }}
+        py={8}
+      >
+        <HStack width="100%" px={4}>
+          <HStack space={2} alignItems="center">
+            <Avatar color="white" bg={"secondary.700"}>
+              <Text>{index}</Text>
+            </Avatar>
+            <Text>{item.title}</Text>
+          </HStack>
+        </HStack>
+      </Pressable>
+    </Box>
+  );
+};
+
+export default Item;
--- src/screens/Favorites/index.js
+++ src/screens/Favorites/index.js
@@ -1,8 +1,7 @@
-import React, { useEffect, useState, useRef } from "react";
+import React, { useEffect, useState} from "react";
 import { useColorMode } from "native-base";
 import {
   getValuesFromStore,
-  removeValueFromStore,
 } from "../../utils/FavoritesStore";
 import ColorCenter from "../../components/ColorableCenter";
 
@@ -19,8 +18,9 @@ import {
   Button,
 } from "native-base";
 import { SwipeListView } from "react-native-swipe-list-view";
-import HiddenItem from "./components/HiddenItem";
 
+import HiddenItem from "./components/HiddenItem";
+import Item from "./components/Item";
 
 const Favorites = () => {
   const { colorMode } = useColorMode();
@@ -37,36 +37,7 @@ const Favorites = () => {
   const renderHiddenItem = (data, rowMap) => {
     return <HiddenItem {...{ data, rowMap, toast, getData}} />;
   };
-  const Item = ({ item, index }) => {
-    const { colorMode } = useColorMode();
-    return (
-      <Box>
-        <Pressable
-          onPress={() => console.log("You touched me")}
-          alignItems="center"
-          bg={colorMode === "dark" ? "dark.200" : "white"}
-          borderBottomColor="trueGray.200"
-          borderBottomWidth={1}
-          justifyContent="center"
-          height={50}
-          underlayColor={"#AAA"}
-          _pressed={{
-            bg: "trueGray.200",
-          }}
-          py={8}
-        >
-          <HStack width="100%" px={4}>
-            <HStack space={2} alignItems="center">
-              <Avatar color="white" bg={"secondary.700"}>
-                <Text>{index}</Text>
-              </Avatar>
-              <Text>{item.title}</Text>
-            </HStack>
-          </HStack>
-        </Pressable>
-      </Box>
-    );
-  };
+  
   const renderItem = ({ item, index }) => <Item {...{ item, index }} />;
 
   if (!listData.length)
